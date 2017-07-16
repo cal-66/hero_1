2244a2a24f19841523d9ae3ff4703e95f117915a
Carol Craig <carolanne.craig@gmail.com>
2017-07-16 23:42:33 +0100

    about page added

--- routes.py
+++ routes.py
@@ -12,6 +12,10 @@ app = Flask(__name__)
 def index():
 	return render_template("index.html")
 
+@app.route("/about")
+def about():
+	return render_template("about.html")
+
 
 if __name__ == '__main__':
 	#app.run(debug = False, host='0.0.0.0', port=port)
